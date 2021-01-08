870bb10fbdd01feadfba422700571810b33bbb7b
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2021-01-08 20:04:24 +0200

    Folders re-structure

---
+++ storybook/README.md
@@ -0,0 +1,6 @@
+# pickly-v2-starter-template
+
+## How to use?
+
+1. In the command line, do `yarn` to install all the dependencies. 
+3. `yarn start`
---
+++ storybook/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+function App() {
+  return (
+    <div className="font-sans">
+      <section>
+        <h1 className="text-3xl">Add Component(s) here.</h1>
+      </section>
+      <section>
+        <h1 className="text-3xl">Add Component(s) here.</h1>
+      </section>
+    </div>
+  );
+}
+
+export default App;
