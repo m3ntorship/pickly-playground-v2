af01ec93c12ab8a3d2676ad363396c0bdb05c236
Kischa-hub <km.elhosseny@gmail.com>
2021-01-11 17:45:16 +0100

    adding ClassNames Lib & adding Story for a button

---
+++ storybook/src/kareem/Components/Atoms/Button/Button.stories.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Button, BUTTON_OPTIONS } from "./Button";
+
+export default {
+  title: "Button",
+  component: Button,
+  argTypes: {
+    size: {
+      control: {
+        type: "radio",
+        options: [
+          BUTTON_OPTIONS.SIZE.SMALL,
+          BUTTON_OPTIONS.SIZE.MEDIUM,
+          BUTTON_OPTIONS.SIZE.LARGE,
+        ],
+      },
+    },
+  },
+};
+
+const Template = (args) => <Button {...args} />;
+export const Primary = Template.bind({});
+Primary.args = {
+  children: "Button Text",
+  primary: true,
+  size: BUTTON_OPTIONS.SIZE.MEDIUM,
+};
+export const Secondary = Template.bind({});
+Secondary.args = { ...Primary.args, primary: false };
