f176cf32d4103a47cc24f66b8f5a2bb009184ab8
Safei Ashraf <Safei.Ashraf@gmail.com>
2021-01-11 16:39:45 +0200

    merging

--- storybook/src/index.js
+++ storybook/src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./css/index.css";
-import App from "./App";
+// import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>test</div>
   </React.StrictMode>,
   document.getElementById("root")
 );
