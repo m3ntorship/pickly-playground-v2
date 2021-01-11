e0426b546a53ff8177e3768d97d4acb9b8fb9c86
Safei Ashraf <Safei.Ashraf@gmail.com>
2021-01-11 20:26:04 +0200

    adding test file for button

--- storybook/src/Safei/components/Atoms/Buttons/Buttons.test.js
+++ storybook/src/Safei/components/Atoms/Buttons/Buttons.test.js
@@ -1,3 +1,10 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import ArrowDown from "./ArrowDown";
+import PrimaryButtonLG from "./PrimaryButtonLG";
+
+describe("render my primary button", () => {
+  it("render primary button comp large size", () => {
+    const tree = renderer.create(<PrimaryButtonLG />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
