0f6c8bd7727c5bf8d6e77fd9fe54a28d44d3a27f
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2021-01-12 18:34:16 +0200

    Add Checkbox Avatar Button CircleProgress components

---
+++ storybook/src/Sahl/Atoms/Button/Button.js
@@ -0,0 +1,56 @@
+import React from "react";
+import PropTypes from "prop-types";
+import DownArrow from "../../../assets/icons/DownArrow";
+
+function Button(props) {
+  const {
+    disabled = false,
+    size = "md",
+    variant = "primary",
+    leftArrow = false,
+    rightArrow = false,
+    className,
+    children,
+  } = props;
+
+  return (
+    <button
+      disabled={disabled}
+      className={`${className} rounded-full border disabled:opacity-30 flex items-center ${
+        variant === "primary"
+          ? "bg-primary text-white hover:bg-primary-shd9 focus:border-primary-shd9 focus:border-solid focus:outline-none"
+          : "bg-white text-primary border-primary border-solid hover:bg-grey-bg2 focus:border-2 focus:outline-none"
+      } ${
+        size === "lg"
+          ? "py-3.5 px-8"
+          : size === "sm"
+          ? "py-1.5 px-4"
+          : "py-2.5 px-6"
+      }`}
+    >
+      {leftArrow && (
+        <DownArrow
+          path1Fill={variant === "primary" ? "#FFF" : "rgba(112, 72, 232)"}
+        />
+      )}
+      <span>{children}</span>
+      {rightArrow && (
+        <DownArrow
+          path1Fill={variant === "primary" ? "#FFF" : "rgba(112, 72, 232)"}
+        />
+      )}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  size: PropTypes.oneOf(["sm", "md", "lg"]),
+  disabled: PropTypes.bool,
+  leftArrow: PropTypes.bool,
+  rightArrow: PropTypes.bool,
+  variant: PropTypes.oneOf(["primary", "secondary"]),
+  className: PropTypes.string,
+  children: PropTypes.string,
+};
+
+export default Button;
---
+++ storybook/src/Sahl/Atoms/CheckBox/CheckBox.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import CheckIcon from "../../../assets/icons/Check";
+
+export default function CheckBox(props) {
+  const { size = "default", disabled = false } = props;
+  const [checked, setChecked] = useState(false);
+
+  const onChange = () => {
+    setChecked(!checked);
+  };
+  return (
+    <div
+      className={`relative flex justify-center items-center ${
+        size === "default" ? "w-6 h-6 rounded-sm" : "w-4 h-4 rounded-sm"
+      } border border-grey-shd4 hover:border-grey-shd1 ${
+        checked && "bg-primary hover:bg-primary-shd8"
+      } ${disabled && "opacity-30"} focus:outline-none focus:border-2`}
+    >
+      <input
+        disabled={disabled}
+        type="checkbox"
+        onChange={onChange}
+        className="absolute opacity-0 cursor-pointer w-full h-full"
+      />
+      {checked && (
+        <CheckIcon
+          width={size === "default" ? "w-4" : "w-3"}
+          height={size === "default" ? "h-4" : "h-3"}
+          path1Fill="#FFF"
+        />
+      )}
+    </div>
+  );
+}
+
+CheckBox.propTypes = {
+  size: PropTypes.oneOf(["default", "small"]),
+  disabled: PropTypes.bool,
+};
