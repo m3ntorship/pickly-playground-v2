c56e1a919aff1cf0fa5c6ccac7c91f315fb2acf8
Safei Ashraf <Safei.Ashraf@gmail.com>
2021-01-14 19:26:15 +0200

    reworked post options comp

--- storybook/src/Safei/components/Mollecules/PostOptions/PostOptions.js
+++ storybook/src/Safei/components/Mollecules/PostOptions/PostOptions.js
@@ -8,17 +8,20 @@ const PostOptions = () => {
         <Toggle size="sm" />
       </div>
       <div
-        className="text-xs ml-2 leading-4 mb-0 pr-6  border-r border-grey-shd6"
-        style={{ paddingTop: "2px" }}
+        className="text-xs ml-2 leading-4 mb-0 pr-6 "
+        style={{ paddingTop: "3px" }}
       >
         Hide My Identity
       </div>
-      <div
-        style={{ paddingTop: "2px" }}
-        className="text-xs ml-2 leading-4 mb-0 pr-6 cursor-pointer"
-      >
-        <label>Privacy:</label>
-        <select name="privacy" id="privacy" className="cursor-pointer">
+      <div className="text-xs ml-2 leading-4 mb-0 pr-6 cursor-pointer">
+        <label htmlFor="privacy" className="font-normal text-xs text-dark-grey">
+          Privacy:
+        </label>
+        <select
+          name="privacy"
+          id="privacy"
+          className="cursor-pointer text-center ml-2 font-medium outline-none text-sm text-dark-grey"
+        >
           <option value="Public">Public</option>
           <option value="Custom">Custom</option>
           <option value="Private">Private</option>
