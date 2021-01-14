c046d75f9ffdb26bfa2ab07356c998fdca1e7e95
Safei Ashraf <Safei.Ashraf@gmail.com>
2021-01-14 18:26:42 +0200

    adding attrbs to pollTab comp

--- storybook/src/Safei/components/Atoms/Tabs/PollTab.js
+++ storybook/src/Safei/components/Atoms/Tabs/PollTab.js
@@ -7,8 +7,11 @@ const PollTab = () => {
       className="group text-sm relative flex items-center justify-around px-3.5 py-1.5 h-8 border border-grey border-opacity-0 bg-grey-shd7  hover:bg-white hover:border-opacity-100  rounded-full focus:outline-none transition duration-500 ease-in-out"
       style={{ paddingRight: "12px", marginRight: "18px" }}
     >
-      <label className="cursor-pointer relative flex items-center justify-items-center content-center">
-        <input type="radio" name="radio" className="bg-primary" />
+      <label
+        className="cursor-pointer relative flex items-center justify-items-center content-center"
+        htmlFor="poll-option"
+      >
+        <input type="radio" name="poll-option" className="bg-primary" />
         <span className="checkmark h-4 w-4 rounded-full border border-primary  bg-white text-transparent absolute left-0 top-1 mr-2 hover:bg-grey-shd4 transition duration-500 ease-in-out">
           {" "}
         </span>
