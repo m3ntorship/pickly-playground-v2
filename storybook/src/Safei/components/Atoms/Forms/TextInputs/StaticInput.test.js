7d868345a333f18334de4a45a9977f78cd596e76
Safei Ashraf <Safei.Ashraf@gmail.com>
2021-01-16 01:05:22 +0200

    updating test cases for files submitted

--- storybook/src/Safei/components/Atoms/Forms/TextInputs/StaticInput.test.js
+++ storybook/src/Safei/components/Atoms/Forms/TextInputs/StaticInput.test.js
@@ -8,3 +8,31 @@ describe("Label Input field", () => {
     expect(tree).toMatchSnapshot();
   });
 });
+
+describe("Label Input field", () => {
+  it("renders a marked input field in normal state with home icon ", () => {
+    const tree = renderer
+      .create(<StaticInput status={"normal"} icon={"home"} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("renders a marked input field with error icon and prefix +20", () => {
+    const tree = renderer
+      .create(<StaticInput status={"error"} prefix={"+20"} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("renders a marked input field with success icon + home icon + prefix =+20  ", () => {
+    const tree = renderer
+      .create(<StaticInput status={"success"} icon={"home"} prefix={"+20"} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("renders a marked input field disabled with default set up  ", () => {
+    const tree = renderer.create(<StaticInput disabled={true} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
