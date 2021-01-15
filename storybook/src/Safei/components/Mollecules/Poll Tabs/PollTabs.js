6aef479d4c67954a66c6ffd1010d4cdea14ff1e1
Safei Ashraf <Safei.Ashraf@gmail.com>
2021-01-15 21:50:46 +0200

    adjusting poll tab styles

--- storybook/src/Safei/components/Mollecules/Poll Tabs/PollTabs.js
+++ storybook/src/Safei/components/Mollecules/Poll Tabs/PollTabs.js
@@ -1,14 +1,14 @@
 import React from "react";
 import PollTab from "../../Atoms/Tabs/PollTab";
 
-const PollTabs = ({ count }) => {
-  const tabsArr = [];
-  if (!count) count = 1; //set default value;
-  tabsArr.length = count;
-  tabsArr.fill(<PollTab />);
+const PollTabs = () => {
   return (
     <div className="inline-block">
-      <div className="flex flex-row">{tabsArr}</div>
+      <div className="flex flex-row">
+        <PollTab type={"img"} text={"Img Poll"} />
+        <PollTab type={"file"} text={"File Poll"} />
+        <PollTab type={"poll"} text={"A Poll Poll"} />
+      </div>
     </div>
   );
 };
--- storybook/src/Safei/components/Mollecules/Poll Tabs/PollTabs.stories.js
+++ storybook/src/Safei/components/Mollecules/Poll Tabs/PollTabs.stories.js
@@ -4,19 +4,19 @@ import PollTabs from "./PollTabs";
 export default {
   title: "Mollecules/PollTabs",
   component: PollTabs,
-  count: {
-    control: {
-      type: "inline-radio",
-      options: [0, 1, 2, 3],
-    },
-  },
+  // count: {
+  //   control: {
+  //     type: "inline-radio",
+  //     options: [0, 1, 2, 3],
+  //   },
+  // },
 };
 
 const Template = (args) => <PollTabs {...args} />;
 export const PollTabsDisplay = Template.bind({});
 //could add number of tabs as prop
 PollTabsDisplay.args = {
-  count: 3,
+  // count: 3,
   // prefix: '+20',
   // direction: 'flex-row'
 };
