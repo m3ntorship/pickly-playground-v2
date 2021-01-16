5a917436faac3948fa3fb56aede8fae1e3d77267
EmanFouad <engemanf11@gmail.com>
2021-01-16 18:18:08 +0200

    changes in button-icon-test

--- storybook/src/Eman/Atoms/Buttons/Primary-Icon/Button-icon.test.js
+++ storybook/src/Eman/Atoms/Buttons/Primary-Icon/Button-icon.test.js
@@ -15,15 +15,50 @@ describe("Testing ButtonIcon", () => {
     const tree = renderer.create(<ButtonIcon size="small" />).toJSON();
     expect(tree).toMatchSnapshot();
   });
-  it("Testing ButtonIcon iconPosition", () => {
+  it("Testing ButtonIcon iconPosition left", () => {
     const tree = renderer.create(<ButtonIcon iconPosition="left" />).toJSON();
     expect(tree).toMatchSnapshot();
   });
-  it("Testing ButtonIcon iconPosition", () => {
+  it("Testing ButtonIcon iconPosition right", () => {
     const tree = renderer.create(<ButtonIcon iconPosition="right" />).toJSON();
     expect(tree).toMatchSnapshot();
   });
-
+  it("Testing ButtonIcon size big iconPosition left", () => {
+    const tree = renderer
+      .create(<ButtonIcon size="big" iconPosition="left" />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  it("Testing ButtonIcon size medium iconPosition left", () => {
+    const tree = renderer
+      .create(<ButtonIcon size="medium" iconPosition="left" />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  it("Testing ButtonIcon size small iconPosition left", () => {
+    const tree = renderer
+      .create(<ButtonIcon size="small" iconPosition="left" />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  it("Testing ButtonIcon size big iconPosition right", () => {
+    const tree = renderer
+      .create(<ButtonIcon size="big" iconPosition="right" />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  it("Testing ButtonIcon size medium iconPosition right", () => {
+    const tree = renderer
+      .create(<ButtonIcon size="medium" iconPosition="right" />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  it("Testing ButtonIcon size small iconPosition right", () => {
+    const tree = renderer
+      .create(<ButtonIcon size="small" iconPosition="right" />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
   it("Testing ButtonIcon with no props", () => {
     const tree = renderer.create(<ButtonIcon />).toJSON();
     expect(tree).toMatchSnapshot();
