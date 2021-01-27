fe77e8e417bc06897d4b689eb175ba3c0257d494
Ahmed Twfiek <ahmed.twfiek8@gmail.com>
2021-01-27 11:59:07 +0200

    added atoms {form input}

---
+++ storybook/src/Twfiek/Atoms/FormInput/components/Input.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Input = (props) => {
+  const {
+    leftIcon,
+    prefix,
+    prefixDrop,
+    variant,
+    disabled,
+    click,
+    setHover,
+  } = props;
+  return (
+    <input
+      className={`w-72 h-3xl py-2.5 pr-m ${
+        leftIcon ? "pl-9" : prefix ? "pl-3xl" : prefixDrop ? "pl-4xl" : "pl-m"
+      } text-sm text-grey-shd1 font-normal border rounded-md hover:border-grey-shd2 focus:text-dark-grey ${
+        variant === "error"
+          ? "border-error focus:border-error"
+          : variant === "success"
+          ? "border-success focus:border-success"
+          : "border-grey-shd5 focus:border-dark"
+      } focus:outline-none ${
+        disabled
+          ? "pointer-events-none opacity-50 border-none border-grey-shd5"
+          : ""
+      }`}
+      type="text"
+      placeholder="Enter text"
+      onClick={click}
+      onMouseEnter={() => (leftIcon ? setHover(true) : null)}
+      onMouseLeave={() => (leftIcon ? setHover(false) : null)}
+    />
+  );
+};
+
+export default Input;
