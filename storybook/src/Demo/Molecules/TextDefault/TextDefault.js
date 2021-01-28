e88eaf9855841fd4bbd81b3360c5a2b8bc8a25cf
Ahmed Twfiek <ahmed.twfiek8@gmail.com>
2021-01-28 12:14:17 +0200

    fixed removeItem function, made letters and numbers dynamic changes, show remove icon after adding more than two options

--- storybook/src/Demo/Molecules/TextDefault/TextDefault.js
+++ storybook/src/Demo/Molecules/TextDefault/TextDefault.js
@@ -3,29 +3,37 @@ import AddOption from "./components/AddOption";
 import OptionInput from "./components/OptionInput";
 
 const TextDefault = () => {
-  const [addOption, setAddOption] = useState([]);
-  const alpha = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
+  const [addOption, setAddOption] = useState([{ id: 1 }, { id: 2 }]);
+  // const alpha = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
+  //  Alphabet letters
+  const letters = (() => {
+    const caps = [...Array(26)].map((val, i) => String.fromCharCode(i + 65));
+    return caps;
+  })();
   const addOptionHandler = () => {
     let id = addOption.length + 1;
-    if (addOption[addOption.length - 1] !== alpha[alpha.length - 1]) {
-      setAddOption([...addOption, { id: id, alpha: alpha[id - 1] }]);
-    }
+    //alpha: alpha[id - 1]
+    setAddOption([...addOption, { id: id }]);
   };
   const removeOptionHandler = (e) => {
-    addOption.filter((item) => item.id !== e.target.id);
-    console.log(addOption);
+    setAddOption(addOption.filter((item) => item.id != e.target.id));
   };
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col w-4/12">
       <div className="flex flex-col w-full">
-        {addOption.map((item, index) => (
-          <OptionInput
-            key={index}
-            id={item.id}
-            alpha={item.alpha}
-            click={removeOptionHandler}
-          />
-        ))}
+        {addOption.map((item, index) => {
+          const letter = letters[index];
+          return (
+            <OptionInput
+              key={index}
+              id={item.id}
+              alpha={letter}
+              click={removeOptionHandler}
+              index={index}
+              addOption={addOption}
+            />
+          );
+        })}
       </div>
       <AddOption click={addOptionHandler} />
     </div>
--- storybook/src/Demo/Molecules/TextDefault/components/OptionInput.js
+++ storybook/src/Demo/Molecules/TextDefault/components/OptionInput.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const OptionInput = ({ id, alpha, click }) => {
+const OptionInput = ({ id, alpha, click, index, addOption }) => {
   return (
     <div className="mb-xs flex w-full items-center">
       <svg
@@ -17,7 +17,7 @@ const OptionInput = ({ id, alpha, click }) => {
         <input
           className="hover:border-grey-shd2 focus:text-dark-grey focus:border-dark border border-grey-shd5 bg-transparent md:bg-white py-2.5 pr-m pl-11 text-sm font-normal text-grey-shd1 w-full rounded-md focus:outline-none"
           type="text"
-          placeholder={`Option ${id}`}
+          placeholder={`Option ${index + 1}`}
         />
         <div className="bg-grey-shd7 py-0.5 px-xs rounded-sm absolute top-2 left-2">
           <h3 className="text-sm text-grey font-normal leading-snug">
@@ -25,27 +25,29 @@ const OptionInput = ({ id, alpha, click }) => {
           </h3>
         </div>
       </div>
-      <svg
-        className="ml-s cursor-pointer"
-        width="16"
-        height="16"
-        viewBox="0 0 16 16"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        onClick={click}
-        id={id}
-      >
-        <path
-          d="M4.66675 7.3335H11.3334V8.66683H4.66675V7.3335Z"
-          fill="#FE544A"
+      {addOption.length > 2 ? (
+        <svg
+          className="ml-s cursor-pointer"
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          onClick={click}
           id={id}
-        />
-        <path
-          d="M7.99992 1.3335C4.32392 1.3335 1.33325 4.32416 1.33325 8.00016C1.33325 11.6762 4.32392 14.6668 7.99992 14.6668C11.6759 14.6668 14.6666 11.6762 14.6666 8.00016C14.6666 4.32416 11.6759 1.3335 7.99992 1.3335ZM7.99992 13.3335C5.05925 13.3335 2.66659 10.9408 2.66659 8.00016C2.66659 5.0595 5.05925 2.66683 7.99992 2.66683C10.9406 2.66683 13.3333 5.0595 13.3333 8.00016C13.3333 10.9408 10.9406 13.3335 7.99992 13.3335Z"
-          fill="#FE544A"
-          id={id}
-        />
-      </svg>
+        >
+          <path
+            d="M4.66675 7.3335H11.3334V8.66683H4.66675V7.3335Z"
+            fill="#FE544A"
+            id={id}
+          />
+          <path
+            d="M7.99992 1.3335C4.32392 1.3335 1.33325 4.32416 1.33325 8.00016C1.33325 11.6762 4.32392 14.6668 7.99992 14.6668C11.6759 14.6668 14.6666 11.6762 14.6666 8.00016C14.6666 4.32416 11.6759 1.3335 7.99992 1.3335ZM7.99992 13.3335C5.05925 13.3335 2.66659 10.9408 2.66659 8.00016C2.66659 5.0595 5.05925 2.66683 7.99992 2.66683C10.9406 2.66683 13.3333 5.0595 13.3333 8.00016C13.3333 10.9408 10.9406 13.3335 7.99992 13.3335Z"
+            fill="#FE544A"
+            id={id}
+          />
+        </svg>
+      ) : null}
     </div>
   );
 };
